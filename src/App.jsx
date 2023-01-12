45c6ad0fefee3dbe1ae71ed3b0b94cade130dd6b
bstrihova <bara@MacBook-Pro.local>
2023-01-12 11:48:29 +0100

    ensure responsive MUI font sizes in the entire App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './containers/Layout';
 import Home from './containers/Home';
 import Character from './containers/Character';
+import {
+    createTheme,
+    responsiveFontSizes,
+    ThemeProvider,
+} from '@mui/material/styles';
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 function App() {
     const [loggedIn, setLoggedIn] = useState(false);
@@ -14,36 +22,38 @@ function App() {
     }, []);
 
     return (
-        <Router>
-            <Layout loggedIn={loggedIn} setLoggedIn={setLoggedIn}>
-                {/* A <Routes> looks through its children <Route>s and
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Layout loggedIn={loggedIn} setLoggedIn={setLoggedIn}>
+                    {/* A <Routes> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-                <Routes>
-                    {pageLoaded && (
-                        <Fragment>
-                            <Route
-                                exact
-                                path="/character/:characterId"
-                                element={<Character loggedIn={loggedIn} />}
-                            />
-                            <Route
-                                path="/"
-                                element={
-                                    <Home
-                                        setLoggedIn={setLoggedIn}
-                                        loggedIn={loggedIn}
-                                    />
-                                }
-                            />
-                            <Route
-                                path="*"
-                                element={<p>There's nothing here: 404!</p>}
-                            />
-                        </Fragment>
-                    )}
-                </Routes>
-            </Layout>
-        </Router>
+                    <Routes>
+                        {pageLoaded && (
+                            <Fragment>
+                                <Route
+                                    exact
+                                    path="/character/:characterId"
+                                    element={<Character loggedIn={loggedIn} />}
+                                />
+                                <Route
+                                    path="/"
+                                    element={
+                                        <Home
+                                            setLoggedIn={setLoggedIn}
+                                            loggedIn={loggedIn}
+                                        />
+                                    }
+                                />
+                                <Route
+                                    path="*"
+                                    element={<p>There's nothing here: 404!</p>}
+                                />
+                            </Fragment>
+                        )}
+                    </Routes>
+                </Layout>
+            </Router>
+        </ThemeProvider>
     );
 }
 
